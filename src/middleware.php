ea639a33e940f2b1dc2b77b3d2f9d0a4094c47e4
shiwan66 <shiwan66@gmail.com>
2017-09-19 01:00:59 +0800

    change

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,22 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+namespace My;
+
+use Psr\Http\Message\RequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+class Middleware
+{
+    function __invoke(Request $req,  Response $res, callable $next) {
+        // Do stuff before passing along
+        $newResponse = $next($req, $res);
+        $res->write('hllo');
+        // Do stuff after route is rendered
+        return $newResponse; // continue
+    }
+}
+
+
+// Register
+$app->add(new Middleware());
--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,41 @@
 <?php
+use Slim\Http\Stream;
 // Routes
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+// $app->get('/[{name}]', function ($request, $response, $args) {
+//     // Sample log message
+//     $this->logger->info("Slim-Skeleton '/' route");
 
-    // Render index view
+//     // Render index view
+//     return $this->renderer->render($response, 'index.phtml', $args);
+// });
+
+$app->get('/info', function($request, $response, $args) {
     return $this->renderer->render($response, 'Script1.php', $args);
+});
+
+$app->get('/', function($req, $res, $args) {
+    $id= $req->getParam('id');
+    return $res->withStatus(200)->write($id);
+});
+
+$app->get('/redirect', function($req, $res, $args) {
+    return $res->withStatus(302)->withHeader('Location', '/');
+});
+
+$app->get('/homes', function ($request, $response, $args) {
+    $url = $this->router->pathFor('homes');
+    $response->write("<a href='$url'>Home</a>");
+    return $response;
+})->setName('homes');
+
+$app->get('/imgs', function($req, $response, $args) {
+    $image = __DIR__.'/public/img/demo.jpg';
+    $body = new Stream($image);
+    // $response = (new Response())
+    //      ->withStatus(200, 'OK')
+    //      ->withHeader('Content-Type', 'image/jpeg')
+    //      ->withHeader('Content-Length', filesize($image))
+    //      ->withBody($body);
+    return $response->write($image);
 });
